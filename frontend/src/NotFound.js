296647def830ee6993d093fe20c3764a5272e44e
maluyacode <adlawandavemerc98@gmail.com>
2023-09-08 16:37:33 +0800

    not-found-page

---
+++ frontend/src/NotFound.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const NotFound = () => {
+    const styles = {
+        textAlign: "center",
+        margin: "auto",
+        marginTop: "15rem"
+    }
+    return (
+        <h1 style={styles}>404 Not Found</h1>
+    )
+}
+
+export default NotFound

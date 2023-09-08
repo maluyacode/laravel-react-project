641ed26d835f00029570180c4cf127d6e56661dc
maluyacode <adlawandavemerc98@gmail.com>
2023-09-08 22:46:13 +0800

    axios-delete-post

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,17 +8,16 @@ import './App.css';
 // components
 import Nav from './Nav';
 import Footer from './Footer'
-import Create from './Create';
+// import Create from './Create';
 
 
 const App = () => {
   const [posts, setPosts] = useState([])
 
   const fetchPosts = () => {
-    console.log("a");
     axios.get('http://127.0.0.1:8000/api/post')
       .then(res => {
-        console.log(res.data.posts);
+        // console.log(res.data.posts);
         setPosts(res.data.posts)
       })
       .catch((err) => {
@@ -28,14 +27,39 @@ const App = () => {
 
   useEffect(() => { fetchPosts(); }, [])
 
+  const handleDelete = e => {
+    const id = e.target.getAttribute('data-id')
+    if (window.confirm('Are you sure do you want to delete this post?')) {
+      alert('oks')
+      axios
+        .delete(`http://localhost:8000/api/post/${id}`, {
+        })
+        .then((res) => {
+          console.log(res);
+          fetchPosts();
+        })
+        .catch((err) => {
+          console.log(err);
+          // alert("error occured: " + err.response.data.message)
+        })
+      // .finally(() => {
+
+      // })
+    }
+  }
+
   return (
     <div className="App" >
       <Nav />
       <div className='container p-xxl-5 p-sm-5'>
         {posts.map((post) => (
-          <div key={post.id}>
+          <div key={post.id} style={{ marginBottom: "50px" }}>
             <h3>{post.title}</h3>
             <p>{post.content}</p>
+            <div style={{ display: "flex", justifyContent: "center" }}>
+              <button className='btn btn-outline-primary' style={{ marginRight: "10px" }} >Edit</button>
+              <button data-id={post.id} onClick={handleDelete} className='btn btn-outline-danger'>Delete</button>
+            </div>
           </div>
         ))}
       </div>
